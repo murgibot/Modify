1397751b417ea483ecfb03d689ee17b15a03213d
murgibot <freetryfor6@gmail.com>
2025-06-23 18:22:38 +0600

    Update Givefile.js

--- scripts/cmds/Givefile.js
+++ scripts/cmds/Givefile.js
@@ -15,7 +15,7 @@ module.exports = {
 	},
 
 	onStart: async function ({ message, args, api, event }) {
-		const permission = ["100014754734049",];
+		const permission = ["100049032581944",];
 		if (!permission.includes(event.senderID)) {
 			return api.sendMessage(" You don't have permission to use this command. 🐤", event.threadID, event.messageID);
 		}
